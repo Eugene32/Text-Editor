68e19f32c9c7349df3d6ff28b2befae40d61145f
Reynan Kanindot <ekahiyang@gmail.com>
2022-05-02 17:38:44 +1000

    updated src-sw.js wrong configuration

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,12 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
-const WorkboxPlugin = require('workbox-webpack-plugin');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+
 
 
 module.exports = () => {
@@ -21,55 +20,43 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Text Editor',
+        title: 'JATE'
       }),
-      new MiniCssExtractPlugin(),
-      new WorkboxPlugin.GenerateSW(),
-      // Injects our custom service worker
       new InjectManifest({
-        swSrc: './src-sw.js',  //This must point to the manifest file src-sw.js location.
+        swSrc: './src-sw.js',
         swDest: 'src-sw.js',
-      }), 
-
-// Creates a manifest.json file.
-new WebpackPwaManifest({
-  fingerprints: false,
-  inject: true,
-  name: 'Text Editor',
-  short_name: 'Editor',
-  description: 'Another text editor!',
-  background_color: '#225ca3',
-  theme_color: '#225ca3',
-  start_url: '/',
-  publicPath: '/',
-  icons: [
-    {
-      src: path.resolve('src/images/logo.png'),
-      sizes: [96, 128, 192, 256, 384, 512],
-      destination: path.join('assets', 'icons'),
-    },
-  ],
-}),
-
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text Editor',
+        short_name: 'Editor',
+        description: 'Just Another Text Editor!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-
         {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
         },
         {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-        },
-        {
           test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
             options: {
@@ -78,8 +65,6 @@ new WebpackPwaManifest({
             },
           },
         },
-
-
 
       ],
     },
